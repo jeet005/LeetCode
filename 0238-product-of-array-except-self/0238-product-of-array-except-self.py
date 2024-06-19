1522b847a8a2ec3f4f4dbc554ea20a5a7d8b65c5
Jeet <88142996+jeet005@users.noreply.github.com>
2024-06-19 18:50:05 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        product = 0
+        for idx in range(0, len(nums)):
+            product *= nums[idx] * nums[idx + 1]
+
+        answer = [i = ]
+        return answer
+        
