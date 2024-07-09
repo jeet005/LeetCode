58f12528031e955972551f7403f1a52749135b79
Jeet <88142996+jeet005@users.noreply.github.com>
2024-07-09 16:40:31 -0400

    Time: 718 ms (5.04%), Space: 17 MB (64.93%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -3,13 +3,14 @@ class Solution:
         result = [[], []]
     
         for i in range(max(len(nums1), len(nums2))):
-            if nums1[i] and nums1[i] not in nums2:
-                if nums1[i] not in result[0]:
-                    result[0].append(nums1[i])
-            
-            if nums2[i] and nums2[i] not in nums1:
-                if nums2[i] not in result[1]:
-                    result[1].append(nums2[i])
+            if i < len(nums1):
+                if nums1[i] not in nums2:
+                    if nums1[i] not in result[0]:
+                        result[0].append(nums1[i])
+            if i < len(nums2):
+                if nums2[i] not in nums1:
+                    if nums2[i] not in result[1]:
+                        result[1].append(nums2[i])
 
         return result
         
