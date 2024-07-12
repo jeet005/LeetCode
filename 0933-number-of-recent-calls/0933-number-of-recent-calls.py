6efaf6873282161ce1fe9febbc514b93365eee22
Jeet <88142996+jeet005@users.noreply.github.com>
2024-07-11 22:21:02 -0400

    Time: 189 ms (70.71%), Space: 22.2 MB (29.42%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,26 +1,15 @@
 class RecentCounter:
     
     def __init__(self):
-        self.count = []        
+        self.count = deque()
+        self.cnt = 0  
 
     def ping(self, t: int) -> int:
-        cnt = 0
         self.count.append(t)
-        Crange = [t - 3000, t]
-        
-        for x in self.count:
-            if x >= Crange[0] and x <= Crange[1]:
-                cnt += 1
-
-        return cnt
-    
-    def __repr__(self):
-        return '[]'
-
-        return cnt
-        
-        
-
+        # Remove elements that are outside the range [t-3000, t]
+        while self.count[0] < t - 3000:
+            self.count.popleft()
+        return len(self.count)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
