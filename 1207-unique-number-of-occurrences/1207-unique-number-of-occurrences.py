2cf4d56e9fbea2d411b4d16a7291417859814201
Jeet <88142996+jeet005@users.noreply.github.com>
2024-07-11 21:00:42 -0400

    Time: 42 ms (52.75%), Space: 16.6 MB (49.31%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,18 +1,24 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        hmap = {}
+        arr.sort()
+        v = []
 
-        for i in arr:
-            if i in hmap:
-                hmap[i] += 1
-            else:
-                hmap[i] = 1
-            
-        hmap1 = []
-        for key, val in hmap.items():
-            if val in hmap1:
+        i = 0
+        while i < len(arr):
+            cnt = 1
+
+            # Count occurrences of the current element
+            while i + 1 < len(arr) and arr[i] == arr[i + 1]:
+                cnt += 1
+                i += 1
+
+            v.append(cnt)
+            i += 1
+
+        v.sort()
+
+        for i in range(1, len(v)):
+            if v[i] == v[i - 1]:
                 return False
-            else:
-                hmap1.append(val)
-        
+
         return True
