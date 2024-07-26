668f43fec06228fb3e1e030fe5793b8c7e8c64b8
Jeet <88142996+jeet005@users.noreply.github.com>
2024-07-26 12:29:42 -0400

    Time: 50 ms (5.81%), Space: 16.6 MB (13.55%) - LeetHub

---
+++ 3062-winner-of-the-linked-list-game/3062-winner-of-the-linked-list-game.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def gameResult(self, head: Optional[ListNode]) -> str:
+        
+        even = head
+        even_points = 0
+        odd_points = 0
+
+        # Traverse through the linked list assigning points
+        while even is not None:
+            odd = even.next
+            if even.val > odd.val:
+                even_points += 1
+            else:
+                odd_points += 1
+            even = odd.next
+            
+        # Return the winning team
+        if odd_points > even_points:
+            return "Odd"
+        elif odd_points < even_points:
+            return "Even"
+        else:
+            return "Tie"
+
+        
