e8894722614fff845d092e11e236e3d955edaa5c
Jeet <88142996+jeet005@users.noreply.github.com>
2024-08-29 16:15:44 -0400

    Time: 27 ms (9%), Space: 11.8 MB (8.95%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -6,11 +6,11 @@ class Solution(object):
         :rtype: List[bool]
         """
         output = []
-        for i in range(len(candies)):
-            if candies[i] + extraCandies >= max(candies):
-                output.append(True)
-            else:
-                output.append(False)
+        maxCandies = max(candies)
+        for candy in candies:
+            output.append(candy + extraCandies >= maxCandies)
+        
+        return output
 
         return output
         
