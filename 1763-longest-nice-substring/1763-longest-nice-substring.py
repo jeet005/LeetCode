70a36632a90bf894a868bc274fbf5bb9e3047383
Jeet <88142996+jeet005@users.noreply.github.com>
2024-11-14 13:09:36 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,25 +1,21 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
+        if len(s) < 2:
+            return ""
+    
+        # Check characters in the current string
+        unique_chars = set(s)
+        for i, char in enumerate(s):
+            # If a character does not have both cases in the string, split around it
+            if char.swapcase() not in unique_chars:
+                # Divide and conquer: solve for both parts around the invalid character
+                left = Solution.longestNiceSubstring(s[:i])
+                right = Solution.longestNiceSubstring(s[i+1:])
+                # Return the longest nice substring found in either half
+                return left if len(left) >= len(right) else right
 
-        def nice_string(sub):
-            ss = set(sub)
-            for char in sub:
-                if char.swapcase() not in ss:
-                    return False
-            return True
-
-
-
-        longest_str = ''
-        for i in range(len(s)):
-            for r in range(i+1, len(s) + 1):
-                current_str = s[i:r]
-                if nice_string(s[i:r]):
-                    if len(current_str) > len(longest_str):
-                        longest_str = current_str
-
-        
-        return longest_str
+        # If all characters are "nice," return the current substring
+        return s
 
 
             
