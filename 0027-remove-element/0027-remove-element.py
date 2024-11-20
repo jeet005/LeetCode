21ced77476ed5695dfdbecebd9b27578ffac26b3
Jeet <88142996+jeet005@users.noreply.github.com>
2024-11-19 22:23:40 -0500

    Time: 0 ms (100%), Space: 16.5 MB (62.65%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,25 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+        left = 0
+        right = len(nums) - 1
         
+        while left <= right:
+            if nums[left] != val:
+                left += 1
+            elif nums[right] == val:
+                right -= 1
+            elif nums[left] == val and nums[right] != val:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+                left += 1
+
+        return left
+
+
+
+                
+
+
+
+# approach 1: create a array and keep on appending if nums[i] != val (this is wrong as we need in-place)
+# approach 2: Use two prointer to achieve O(1) space complexity
