579e4e593d81140bf34979153fcb7a92e86c4d36
Jeet <88142996+jeet005@users.noreply.github.com>
2024-11-20 21:43:49 -0500

    Time: 48 ms (50.12%), Space: 20.3 MB (45.33%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,22 +1,29 @@
 class MyHashMap:
 
     def __init__(self):
-        self.max = 10
+        self.max = 100
         self.arr = [[] for x in range(self.max)]
+        
+    def hash(self, key):
+        h = 0
+        ss = str(key)
+        for x in ss:
+            h += ord(x)
+        return h % self.max
 
     def put(self, key: int, value: int) -> None:
         h = key % self.max
 
         for idx, item in enumerate(self.arr[h]):
             if item[0] == key:
-                self.arr[h][idx] = (key, value)  # Update the existing key
+                self.arr[h][idx] = (key, value)
                 return
 
-        # If key doesn't exist, append a new key-value pair
         self.arr[h].append((key, value))
-    
+        
     def get(self, key: int) -> int:
         h = key % self.max
+
         for idx, item in enumerate(self.arr[h]):
             if item[0] == key:
                 return item[1]
