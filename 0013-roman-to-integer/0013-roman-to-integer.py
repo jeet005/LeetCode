71f25fbf6f6be821103bb47012aaef10442ccf34
Jeet <88142996+jeet005@users.noreply.github.com>
2024-11-28 11:44:53 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,14 +1,26 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hmap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        dict = {'I' : 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
 
-        output = 0
+        
+        res = 0
+        left = 0
+        while left < len(s):
+            if (s[left] == 'I' and s[left + 1] == 'V') or (s[left] == 'I' and s[left + 1] == 'X') or (s[left] == 'X' and s[left + 1] == 'L') or (s[left] == 'X' and s[left + 1] == 'C') or (s[left] == 'C' and s[left + 1] == 'D') or (s[left] == 'C' and s[left + 1] == 'M'):
+                res += dict[str(s[left] + s[left+1])] 
+                left += 2
+                continue
 
-        for i in range(len(s)):
-            if i < len(s) - 1 and hmap[s[i]] < hmap[s[i+1]]:
-                output -= hmap[s[i]]
-            else:
-                output += hmap[s[i]]
-        
-        return output
-        
+            res += dict[s[left]] 
+            left += 1
+        return res
+
+
+
+
+
+
+
+#logic
+#save the roman numbers in a dict
+# before adding check for edge cases like IV, IX and other
