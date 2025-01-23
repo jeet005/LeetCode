46f497d3152bdfe911dc7b7b84849d27da0209b6
sarabjeet <88142996+jeet005@users.noreply.github.com>
2025-01-22 16:34:04 -0800

    Time: 7 ms (81.58%), Space: 19.8 MB (7.04%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,18 +1,10 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        l = 0
-        n = len(nums)
-        r = len(nums) - 1
-        result = [0] * n
 
-        for i in range(n - 1, -1, -1):
-            if nums[l] < nums[r]:
-                square = nums[r]
-                r -= 1
-            else:
-                square = nums[l]
-                l += 1
+        squares = []
 
-            result[i] = square * square
-        
-        return result
+        for num in nums:
+            squares.append(abs(num) * abs(num))
+
+        squares = sorted(squares)
+        return squares
