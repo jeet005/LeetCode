e1b45c05d4f6384c2cfcc3d18d0b7caa82c8efb8
sarabjeet <88142996+jeet005@users.noreply.github.com>
2025-02-05 12:38:54 -0800

    Time: 0 ms (100%), Space: 17.8 MB (61.73%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def gcdOfStrings(self, str1, str2):
-        """
-        :type str1: str
-        :type str2: str
-        :rtype: str
-        """
-        # Check if concatenated strings are equal or not, if not return ""
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1 + str2 != str2 + str1:
             return ""
-        # If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
-        from fractions import gcd
-        return str1[:gcd(len(str1), len(str2))]
-        
+
+        res = gcd(len(str1), len(str2))
+
+        return str2[:res]
