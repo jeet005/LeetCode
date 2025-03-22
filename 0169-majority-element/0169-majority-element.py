28c8d8e962f8bc9891d86e3d0caa9d74c58956d2
sarabjeet <88142996+jeet005@users.noreply.github.com>
2025-03-21 21:29:49 -0700

    Time: 3 ms (82.23%), Space: 19.5 MB (20.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        counts = {}
+        counts = collections.Counter(nums)
+        # counts = {}
         n = len(nums)
-        for num in nums:
-            if num in counts:
-                counts[num] += 1
-            else:
-                counts[num] = 1
+        # for num in nums:
+        #     if num in counts:
+        #         counts[num] += 1
+        #     else:
+        #         counts[num] = 1
 
         for key, val in counts.items():
             if val > n / 2:
