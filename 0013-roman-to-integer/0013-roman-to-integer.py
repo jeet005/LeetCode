f8253b9359270bb295154d3ebea1e92ddc6d62a9
sarabjeet <88142996+jeet005@users.noreply.github.com>
2025-04-09 13:43:25 -0700

    Time: 8 ms (27.79%), Space: 18 MB (21.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,20 +2,14 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         dict = {'I' : 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
 
-        
-        res = 0
-        left = 0
-        while left < len(s):
-            if left < len(s) - 1:
-                if (s[left] == 'I' and s[left + 1] == 'V') or (s[left] == 'I' and s[left + 1] == 'X') or (s[left] == 'X' and s[left + 1] == 'L') or (s[left] == 'X' and s[left + 1] == 'C') or (s[left] == 'C' and s[left + 1] == 'D') or (s[left] == 'C' and s[left + 1] == 'M'):
-                    res += dict[str(s[left] + s[left+1])] 
-                    left += 2
-                    continue
+        total = dict[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if dict[s[i]] < dict[s[i + 1]]:
+                total -= dict[s[i]]
+            else:
+                total += dict[s[i]]
 
-            res += dict[s[left]] 
-            left += 1
-
-        return res
+        return total
 
 
 
